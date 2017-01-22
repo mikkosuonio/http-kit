744e0c4d922b17c32838b81a47bdd88671d0074f
Mikko Suonio <mikko.suonio@solita.fi>
2017-01-22 19:29:28 +0200

    Send an empty pong frame as a response.

--- src/java/org/httpkit/server/HttpServer.java
+++ src/java/org/httpkit/server/HttpServer.java
@@ -154,7 +154,7 @@ public class HttpServer implements Runnable {
                     tryWrite(key, WsEncode(WSDecoder.OPCODE_PONG, frame.data));
                 } else if (frame instanceof PongFrame) {
                     atta.decoder.reset();
-                    tryWrite(key, WsEncode(WSDecoder.OPCODE_PONG, frame.data));
+                    tryWrite(key, WsEncode(WSDecoder.OPCODE_PONG, new byte[0]));
                 } else if (frame instanceof CloseFrame) {
                     handler.clientClose(atta.channel, ((CloseFrame) frame).getStatus());
                     // close the TCP connection after sent
